d7b8e219bfdb20fb54d3fde513722882fe9b305b
shashwat-bajaj <achalbajaj1@gmail.com>
2026-02-03 22:10:29 -0800

    Update inventory.cpp

--- module2/inventory.cpp
+++ module2/inventory.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    cout << "Hello Inventory System!" << endl;
+  
+    cout << "\nEscape Character Demonstration:" << endl;
+    cout << "New Line:\\n -> This is on a new line.\n";
+    cout << "Tab:\\t -> This text is tabbed.\tTabbed text\n";
+    cout << "Double Quote: \"Hello\"" << endl;
+    cout << "Backslash: \\" << endl;
+
+    cout << "\nData Type Sizes (in bytes):" << endl;
+    cout << "int: " << sizeof(int) << endl;
+    cout << "float: " << sizeof(float) << endl;
+    cout << "char: " << sizeof(char) << endl;
+
+    return 0;
+}
